f19fbe4579b592411a7d0c99f47c59ef5a55655d
Jason Wang <vuwij@me.com>
2017-04-27 15:48:08 -0400

    Add project files.

---
+++ mcveillance.net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mcveillance.net.MvcApplication" Language="C#" %>
---
+++ mcveillance.net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mcveillance.net.Startup))]
+namespace mcveillance.net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
